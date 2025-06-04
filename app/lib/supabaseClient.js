b162358390ee2f55cc63e6d0c48db87e26e638dc
Bobi <MrGr33n98@users.noreply.github.com>
2025-06-04 13:52:42 -0300

    supabaSE api key atualziada

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://othmgmckjkdtpfeujdsa.supabase.co';
-// ESTA É A CHAVE CORRETA PARA O FRONTEND (CHAVE ANON / PÚBLICA)
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90aG1nbWNramtkdHBmZXVqZHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDgyMzAsImV4cCI6MjA2NDYyNDIzMH0.Y1S6YTBjfsRNQ-afbal1QIJ6iWjcrjldq0587XoTVj8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
