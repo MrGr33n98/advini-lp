88c242a61c5b9a01fa2c8ea04bcadba50640c104
Bobi <MrGr33n98@users.noreply.github.com>
2025-06-05 00:16:16 -0300

    spa

--- app/lib/supabaseClient.js
+++ app/lib/supabaseClient.js
@@ -1,13 +1,5 @@
-// app/lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error(
-        'Variáveis do Supabase não encontradas. Verifique .env.local'
-    );
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = 'https://qzlsgygzuzvvyuyvcknu.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
